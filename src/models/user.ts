391459abbd42e738a6ec289c381562584061fceb
bhwong11 <user@email.com>
2021-10-10 13:56:18 -0400

    added models

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,10 +4,25 @@ class User{
     static show(id:String|Number){
         return fetch(`${url}/users/${id}`).then(response=>response.json())
     }
-    static update(id:String|Number){
+
+    static update(id:String|Number,data:object){
         return fetch(`${url}/users/${id}`,{
             method:'PUT',
-            body:JSON.stringify(data)
+            body:JSON.stringify(data),
+            headers:{
+                'Content-type':'application/json'
+            }
+        }).then(response=>response.json())
+    }
+
+    static delete(id:String|Number){
+        return fetch(`${url}/users/${id}`,{
+            method:'DELETE',
+            headers:{
+                'Content-type':'application/json'
+            }
         }).then(response=>response.json())
     }
 }
+
+export default User;
